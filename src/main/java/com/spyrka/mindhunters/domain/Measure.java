ce6d00f2a2cbf00f27103a81886a89bd9bb51e53
Lukasz <lukasz.spyrka@gmail.com>
2020-07-09 13:52:25 +0200

    Add Measure entity and connected to database successfully

---
+++ src/main/java/com/spyrka/mindhunters/domain/Measure.java
@@ -0,0 +1,40 @@
+package com.spyrka.mindhunters.domain;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NamedQueries({
+        @NamedQuery(
+                name = "Measure.getByQuantity",
+                query = "SELECT m FROM Measure m where m.quantity= :quantity"
+        )
+})
+
+@Entity
+@Table(name = "measure")
+public class Measure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}

bb3e2043edb1000b981ac19a13da04a5e565cd21
Lukasz <lukasz.spyrka@gmail.com>
2020-07-09 11:16:49 +0200

    Initial commit

---
+++ src/main/java/com/spyrka/mindhunters/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spyrka.mindhunters;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MindhuntersApplication.class);
+    }
+
+}

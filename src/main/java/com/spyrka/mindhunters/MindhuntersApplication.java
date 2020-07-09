cee7fc560eed2d4093c3d6375df1b43d134c99eb
Lukasz <lukasz.spyrka@gmail.com>
2020-07-09 11:16:49 +0200

    Initial commit

---
+++ src/main/java/com/spyrka/mindhunters/MindhuntersApplication.java
@@ -0,0 +1,13 @@
+package com.spyrka.mindhunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindhuntersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindhuntersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spyrka/mindhunters/MindhuntersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spyrka.mindhunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindhuntersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

12a489f8fa87170cb34292742ba849dc48209f22
Lukasz <lukasz.spyrka@gmail.com>
2020-07-10 12:29:57 +0200

    Create UserRepository

---
+++ src/main/java/com/spyrka/mindhunters/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.spyrka.mindhunters.repositories;
+
+
+import com.spyrka.mindhunters.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(@NotNull @Email String email);
+
+}

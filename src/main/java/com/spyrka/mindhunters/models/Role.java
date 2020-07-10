6dda2fc30f1b3b71479a1bfcf9ac3b248cda9fd1
Lukasz <lukasz.spyrka@gmail.com>
2020-07-10 12:24:57 +0200

    Change package name to model

---
+++ src/main/java/com/spyrka/mindhunters/models/Role.java
@@ -0,0 +1,53 @@
+package com.spyrka.mindhunters.models;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@NamedQueries({
+        @NamedQuery(
+                name = "Role.findByRole",
+                query = "SELECT r FROM Role r WHERE r.name=:role"
+        )
+})
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @Valid
+    private List<User> users = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}

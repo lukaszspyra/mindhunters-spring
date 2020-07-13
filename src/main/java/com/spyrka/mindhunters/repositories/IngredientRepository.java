affec1c28a35bacece43850f039a7132a7fb23f8
Lukasz <lukasz.spyrka@gmail.com>
2020-07-13 10:44:04 +0200

    Create IngredientRepository

---
+++ src/main/java/com/spyrka/mindhunters/repositories/IngredientRepository.java
@@ -0,0 +1,14 @@
+package com.spyrka.mindhunters.repositories;
+
+import com.spyrka.mindhunters.models.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+
+    List<Ingredient> findByNameContaining(@NotNull String name);
+
+    Ingredient findByName(@NotNull String name);
+}

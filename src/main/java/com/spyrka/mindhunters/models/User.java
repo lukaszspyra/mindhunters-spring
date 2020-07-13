893b211d35c644a6a58846156e2b97b1aa3b6870
Lukasz <lukasz.spyrka@gmail.com>
2020-07-13 12:02:20 +0200

    Complete implementing methods in UserService

--- src/main/java/com/spyrka/mindhunters/models/User.java
+++ src/main/java/com/spyrka/mindhunters/models/User.java
@@ -16,11 +16,11 @@ import java.util.List;
         ),
         @NamedQuery(
                 name = "User.findFavouritesList",
-                query = "SELECT u.drinks FROM User u WHERE u.email like :email"
+                query = "SELECT u.drinks FROM User u WHERE u.email like ?1"
         ),
         @NamedQuery(
-                name = "User.countFindFavouritesList",
-                query = "SELECT COUNT (ud) FROM User u JOIN u.drinks ud WHERE u.email like :email"
+                name = "User.countPagesFindFavouritesList",
+                query = "SELECT COUNT (ud) FROM User u JOIN u.drinks ud WHERE u.email like ?1"
         ),
         @NamedQuery(
                 name = "User.findByEmail",

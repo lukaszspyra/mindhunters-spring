7d263c8da195f2f7600f3ea2a05b5ff54301fa24
Lukasz <lukasz.spyrka@gmail.com>
2020-07-16 12:00:51 +0200

    Move named queries to drinkRepository and implement Pagination acc to Spring Data

--- src/main/java/com/spyrka/mindhunters/repositories/IngredientRepository.java
+++ src/main/java/com/spyrka/mindhunters/repositories/IngredientRepository.java
@@ -11,4 +11,14 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     List<Ingredient> findByNameContaining(@NotNull String name);
 
     Ingredient findByName(@NotNull String name);
+
+/*    @Override
+    public List<Ingredient> liveSearchIngredientsByName(String partialIngredientName) {
+        Query query = entityManager.createNamedQuery("Ingredient.findIngredientsByPartialName");
+        query.setHint(QueryHints.HINT_PASS_DISTINCT_THROUGH, false);
+
+        query.setMaxResults(LIVE_SEARCH_LIMIT);
+        query.setParameter("partialIngredientName", "%" + partialIngredientName + "%");
+        return query.getResultList();
+    }*/
 }

f2dd72e576ec6432f63cbc424402a13459f92127
Lukasz <lukasz.spyrka@gmail.com>
2020-08-13 11:10:30 +0200

    Add properties fields

---
+++ src/main/resources/start.sql
@@ -0,0 +1,6 @@
+INSERT INTO role (id, name) VALUES (1, 'SUPER_ADMIN');
+INSERT INTO role (id, name) VALUES (2,'ADMIN');
+INSERT INTO role (id, name) VALUES (3, 'USER');
+INSERT INTO role (id, name) VALUES (4, 'GUEST');
+
+INSERT INTO user (name, email, role_id) VALUES ('Mindhunters', 'mindhunters3m@gmail.com', 1);

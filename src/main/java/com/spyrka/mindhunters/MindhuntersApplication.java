05fc2bdaf37ea15506362bb79d9c6402fdb49006
Lukasz <lukasz.spyrka@gmail.com>
2020-08-20 11:46:21 +0200

    Add ServletComponentScan annotation to MindhutersApplication class to prepare filters

--- src/main/java/com/spyrka/mindhunters/MindhuntersApplication.java
+++ src/main/java/com/spyrka/mindhunters/MindhuntersApplication.java
@@ -2,8 +2,10 @@ package com.spyrka.mindhunters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class MindhuntersApplication {
 
     public static void main(String[] args) {

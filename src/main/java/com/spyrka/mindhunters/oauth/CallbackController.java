98949e658f0c97eb5ba1543df4507eb32c3bf4b3
lukaszspyra <lukasz.spyrka@gmail.com>
2021-08-16 19:18:02 +0200

    Create Oauth2 google Callback Controller getting user info

---
+++ src/main/java/com/spyrka/mindhunters/oauth/CallbackController.java
@@ -0,0 +1,49 @@
+package com.spyrka.mindhunters.oauth;
+
+import com.spyrka.mindhunters.context.ContextHolder;
+import com.spyrka.mindhunters.models.dto.UserGoogleView;
+import com.spyrka.mindhunters.models.dto.UserView;
+import com.spyrka.mindhunters.services.UserService;
+import com.spyrka.mindhunters.services.mappers.GoogleUserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
+public class CallbackController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallbackController.class.getName());
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private GoogleUserMapper googleUserMapper;
+
+    @GetMapping("/oauth2callback")
+    protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, OAuth2AuthenticationToken token) throws IOException {
+        Map<String, Object> userDataFromGoogle = token.getPrincipal().getAttributes().entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().equalsIgnoreCase("name") || entry.getKey().equalsIgnoreCase("email"))
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        LOGGER.info("User {}, with email {}, logged in", userDataFromGoogle.get("name"), userDataFromGoogle.get("email"));
+
+        UserGoogleView userGoogleView = googleUserMapper.mapGoogleResponseToUserGoogleView(userDataFromGoogle);
+        UserView userView = userService.login(userGoogleView);
+        new ContextHolder(req.getSession(), userView);
+
+        LOGGER.info("User data from google saved to http session");
+        resp.sendRedirect("/");
+    }
+
+}

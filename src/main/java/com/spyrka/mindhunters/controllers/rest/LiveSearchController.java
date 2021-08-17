69709e44db2b315af3536c42371ac7f3a587a937
lukaszspyra <lukasz.spyrka@gmail.com>
2021-08-17 14:45:25 +0200

    Create REST API for Drink Live Search

---
+++ src/main/java/com/spyrka/mindhunters/controllers/rest/LiveSearchController.java
@@ -0,0 +1,26 @@
+package com.spyrka.mindhunters.controllers.rest;
+
+
+import com.spyrka.mindhunters.models.dto.DrinkLiveSearchView;
+import com.spyrka.mindhunters.services.DrinkRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/search")
+public class LiveSearchController {
+
+    @Autowired
+    private DrinkRestService drinkRestService;
+
+    @GetMapping("/drink/{partialName}")
+    public ResponseEntity<List<DrinkLiveSearchView>> drink(@PathVariable("partialName") String partialName) {
+        return ResponseEntity.ok(drinkRestService.findByNameLiveSearch(partialName));
+    }
+
+
+}

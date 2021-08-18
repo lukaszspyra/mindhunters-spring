50125f870e18a243d05c7bccecbee81bcb011e4c
lukaszspyra <lukasz.spyrka@gmail.com>
2021-08-18 03:55:48 +0200

    Create error controller to handle errors showing proper view

---
+++ src/main/java/com/spyrka/mindhunters/controllers/ErrorController.java
@@ -0,0 +1,26 @@
+package com.spyrka.mindhunters.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+
+
+@Controller("/error")
+public class ErrorController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorController.class.getName());
+
+    @GetMapping
+    public String doGet(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+        resp.setContentType("text/html; charset=UTF-8");
+        req.setCharacterEncoding("UTF-8");
+        LOGGER.error("Bad request: {}", req.getPathInfo());
+        return "error";
+
+    }
+}

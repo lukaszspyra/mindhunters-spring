e30a188b971a3cf87dd653e9b7e5c8eb8063f9a2
lukaszspyra <lukasz.spyrka@gmail.com>
2021-08-21 01:49:50 +0200

    Disable csrf protection for post requests by JS
    Spring security was blocking post request done by JS, requiring authorization token

--- src/main/java/com/spyrka/mindhunters/oauth/SecurityConfig.java
+++ src/main/java/com/spyrka/mindhunters/oauth/SecurityConfig.java
@@ -10,6 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().disable();
+        http.csrf().disable();
         http
                 .authorizeRequests()
                 .antMatchers("/oauth_login").authenticated()

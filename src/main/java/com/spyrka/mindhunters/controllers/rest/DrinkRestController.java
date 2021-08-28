383d9857a83d45960aa3aec5f63e4fcbf4f555a0
lukaszspyra <lukasz.spyrka@gmail.com>
2021-08-28 14:11:44 +0200

    Implement DrinkRestController to receive Users requests to delete or update drinks

--- src/main/java/com/spyrka/mindhunters/controllers/rest/DrinkRestController.java
+++ src/main/java/com/spyrka/mindhunters/controllers/rest/DrinkRestController.java
@@ -3,38 +3,25 @@ package com.spyrka.mindhunters.controllers.rest;
 
 import com.spyrka.mindhunters.context.ContextHolder;
 import com.spyrka.mindhunters.models.Drink;
-import com.spyrka.mindhunters.models.dto.FullDrinkView;
 import com.spyrka.mindhunters.services.AdminManagementRecipeService;
-import com.spyrka.mindhunters.services.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Used to receive {@link com.spyrka.mindhunters.models.User} requests to delete or update drink recipes
+ */
 @RestController
 @RequestMapping("/api/drink-management")
 public class DrinkRestController {
 
     @Autowired
-    private DrinkService drinkService;
-
-    @Autowired
     private AdminManagementRecipeService adminManagementRecipeService;
 
-    @GetMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.OK)
-    public ResponseEntity<FullDrinkView> readDrink(@PathVariable("id") Long id) {
-        FullDrinkView foundDrink = drinkService.getFullDrinkViewById(id);
-        if (foundDrink == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(foundDrink);
-    }
-
     /**
-     * Handles user proposals to delete drink from database
+     * Receives user proposals to delete drink from database
      *
      * @param id  of the drink proposed to be deleted
      * @param req in order to get user from session
@@ -50,35 +37,19 @@ public class DrinkRestController {
         return ResponseEntity.status(404).build();
     }
 
-//
-//
-//    @POST
-//    @Path("")
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response add(Drink updateDrink) {
-//
-//        ContextHolder contextHolder = new ContextHolder(request.getSession());
-//        String email = contextHolder.getEmail();
-//
-//        updateDrink.setConfirmUserEmail(email);
-//
-//        if (email != null && drinkService.addOrUpdate(0L, updateDrink,contextHolder)) {
-//            return Response.status(204).build();
-//        } else {
-//            return Response.status(404).build();
-//        }
-//    }
-
-
-    //tu idzie forma po edycji i naciniseciu submit bez approvala
-
+    /**
+     * Receives drink update from user view editDrinkForm, format json is automatically converted by Jackson
+     *
+     * @param id             of drink currently edited
+     * @param submittedDrink with no ID
+     * @param req            containing {@link com.spyrka.mindhunters.models.User} email
+     */
     @PostMapping("/{id}")
-    public ResponseEntity<String> updateDrink(@PathVariable("id") Long id, Drink submittedDrinkUpdate, HttpServletRequest req) {
+    public ResponseEntity<String> updateDrink(@PathVariable("id") Long id, @RequestBody Drink submittedDrink, HttpServletRequest req) {
         ContextHolder contextHolder = new ContextHolder(req.getSession());
         String email = contextHolder.getEmail();
-        submittedDrinkUpdate.setConfirmUserEmail(email);
-        if (email != null && adminManagementRecipeService.addOrUpdateDrink(id, submittedDrinkUpdate)) {
+        submittedDrink.setConfirmUserEmail(email);
+        if (email != null && adminManagementRecipeService.updateDrink(id, submittedDrink)) {
             return ResponseEntity.status(204).build();
         }
         return ResponseEntity.status(404).build();

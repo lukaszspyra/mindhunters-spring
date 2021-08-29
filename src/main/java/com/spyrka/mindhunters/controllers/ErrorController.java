f84440b8b4c33096e73f4876ad27fe54aaf870a3
lukaszspyra <lukasz.spyrka@gmail.com>
2021-08-29 02:24:29 +0200

    Implement specific mapping for errors, disabling whitelabel status page

--- src/main/java/com/spyrka/mindhunters/controllers/ErrorController.java
+++ src/main/java/com/spyrka/mindhunters/controllers/ErrorController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +11,20 @@ import java.io.UnsupportedEncodingException;
 
 
 @Controller
-@RequestMapping("/error")
-public class ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorController.class.getName());
 
-    @GetMapping
+    @GetMapping("/error")
     public String doGet(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
         resp.setContentType("text/html; charset=UTF-8");
         req.setCharacterEncoding("UTF-8");
-        LOGGER.error("Bad request: {}", req.getPathInfo());
+        LOGGER.error("Bad request: {}", req.getRequestURL());
         return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
     }
 }

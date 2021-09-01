4b0ef1f2d8b4c724d8356832858632ebc94deae1
lukaszspyra <lukasz.spyrka@gmail.com>
2021-09-01 11:15:54 +0200

    Replace String concatenation by text blocks

--- src/main/java/com/spyrka/mindhunters/email/AdminEmailBuilder.java
+++ src/main/java/com/spyrka/mindhunters/email/AdminEmailBuilder.java
@@ -15,32 +15,53 @@ public class AdminEmailBuilder implements EmailBuildStrategy {
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("<html>\n" +
-                "  <head>\n" +
-                "    <style>\n" +
-                "      .colored {\n" +
-                "        color: #ffc107;\n" +
-                "      }\n" +
-                "      #body {\n" +
-                "        font-size: 14px;\n" +
-                "      background-color: #3e1010;" +
-                "        font-family: 'Cinzel', serif;" +
-                "      color: #ffc107;}\n" +
-                "    </style>\n" +
-                "  </head>\n" +
-                "  <body>\n" +
-                "    <div id='body'>\n" +
-                "      <p>Dear Admin,</p>\n" +
-                "      <p>Drink recipes awaiting your approval:</p>\n<ul class='colored'>");
+        stringBuilder.append("""
+                <html>
+                                
+                  <head>
+                                
+                    <style>
+                                
+                      .colored {
+                                
+                        color: #ffc107;
+                                
+                      }
+                                
+                      #body {
+                                
+                        font-size: 14px;
+                                
+                      background-color: #3e1010;
+                        font-family: 'Cinzel', serif;
+                      color: #ffc107;}
+                                
+                    </style>
+                                
+                  </head>
+                                
+                  <body>
+                                
+                    <div id='body'>
+                                
+                      <p>Dear Admin,</p>
+                                
+                      <p>Drink recipes awaiting your approval:</p>
+                <ul class='colored'>
+                """);
 
         drinks.forEach(d -> stringBuilder.append("<li>").append(d.toString()).append("</li>"));
 
-        stringBuilder.append("</ul>" +
-                "      <p>Have fun,<br>" +
-                "Mindhunters Virtual Bartender<br>http://mindhunters.jjdd9.is-academy.pl/</p>\n" +
-                "    </div>\n" +
-                "  </body>\n" +
-                "</html>");
+        stringBuilder.append("""
+                </ul>
+                      <p>Have fun,<br>
+                Mindhunters Virtual Bartender<br>http://mindhunters.jjdd9.is-academy.pl/</p>
+                    </div>
+                                
+                  </body>
+                                
+                </html>
+                """);
 
         return stringBuilder.toString();
     }
